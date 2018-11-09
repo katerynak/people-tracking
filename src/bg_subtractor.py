523cc24c125b898279ad72d60a0816d2134ef367
kk <kk@qualcosa.it>
2018-11-09 21:13:55 +0100

    conversion of whites to dark greys

--- src/bg_subtractor.py
+++ src/bg_subtractor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import cv2
 
 
@@ -40,7 +42,11 @@ class Bg_subtractor(object):
         hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(hsv)
         # some bluring added in order to make the background more uniform
-        h = cv2.blur(h, (4, 4))
+
+        h = cv2.blur(h, (10, 5))
+
+        h[h>127] = 0
+
         return h
 
     def fg_mask(self, frame):
@@ -56,3 +62,57 @@ class Bg_subtractor(object):
 
         return foreground
 
+
+if __name__ == "__main__":
+    # background subtraction model
+    bg_sub = Bg_subtractor()
+
+    cap = cv2.VideoCapture('../pedestrians.mp4')
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    # create a named windows and move it
+    cv2.namedWindow('video')
+    cv2.moveWindow('video', 70, 30)
+
+    next_frame, frame = cap.read()
+    play = True
+
+    list = []
+    cnt = 0
+
+    den_frames = 5
+
+    hd = frame
+
+    while cap.isOpened():
+        if play:
+
+            # extract the foreground mask
+            mask = bg_sub.fg_mask(frame)
+
+            # apply it and obtain an rgb foreground
+            col_foreground = cv2.bitwise_and(frame, frame, mask=mask)
+
+            h = bg_sub.rgb2h(frame)
+
+            # h = cv2.fastNlMeansDenoising(h, searchWindowSize = 5)
+
+            cv2.imshow('mask', col_foreground)
+
+            h = cv2.blur(h, (10, 5))
+
+            cv2.imshow('h', h)
+
+            next_frame, frame = cap.read()
+            cnt += 1
+
+        # q for exit, space for pause
+        key = cv2.waitKey(10) & 0xFF
+        if key == ord('q'):
+            break
+        elif key == 0x20:
+            play = not play
+
+    cap.release()
+    cv2.destroyAllWindows()
