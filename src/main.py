9454e3e965ecdf585b9822e26cf0c0e650e184ab
kk <kk@qualcosa.it>
2018-11-12 00:01:12 +0100

    basic object tracking: matching ids based on min distance of bounding boxes centers

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
 import cv2
+import numpy as np
+import time
 
 from bg_subtractor import Bg_subtractor
 from obj_detector import Obj_detector
+from obj_tracker import Obj_tracker
+# from sort import Sort
 from stats import Statistics
 from rectifier import Rectifier
+
 
 # background subtraction model
 bg_sub = Bg_subtractor()
 
 # pedestrians detection model
 ped_det = Obj_detector()
+
+# ped_tr = Sort()
 
 # performance statistics
 stats = Statistics("../groundtruth.txt")
@@ -21,7 +28,7 @@ width = int(cap.get(3))
 height = int(cap.get(4))
 
 # rectifying model
-rect = Rectifier(width, height, 10)
+rect = Rectifier(width, height, shift=35, up=True, central_zoom=30)
 
 # create a named windows and move it
 cv2.namedWindow('video')
@@ -29,6 +36,55 @@ cv2.moveWindow('video', 70, 30)
 
 next_frame, frame = cap.read()
 play = True
+
+# pedestrians tracking model
+ped_tr = Obj_tracker(width, height)
+
+def bbox2coords(bboxes):
+    coords = np.zeros([len(bboxes), 4])
+    for i, bbox in enumerate(bboxes):
+        x1, y1, w, h = bbox
+        x2 = x1 + w
+        y2 = y1 + h
+        coord = np.array([x1,y1,x2,y2])
+        coords[i]=coord
+    return coords
+
+
+def coords2bbox(coords):
+    bboxes = []
+    ids=[]
+    for coord in coords:
+        x1, y1, x2, y2, id = coord
+        ids.append(id)
+        bbox = (int(x1), int(y1), int(x2-x1), int(y2-y1))
+        bboxes.append(bbox)
+    return bboxes, ids
+
+
+def drawBboxes(bboxes, frame, color = (0, 0, 255), ids=None):
+    if ids is not None and len(ids)>0:
+        for (bbox, id) in zip(bboxes, ids):
+            x, y, w, h = bbox
+            # draw a green rectangle to visualize the bounding rect
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, '{}'.format(id),
+                        (x, y),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 3)
+
+    else:
+        for bbox in bboxes:
+            x, y, w, h = bbox
+            # draw a green rectangle to visualize the bounding rect
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+    return frame
+
+#
+# cnt = 0
+# cnt_upd= 2
+# coords = np.array([[0.,   0., 1280.,  720.]])
+
+ids = []
 
 while cap.isOpened() and next_frame:
     if play:
@@ -38,17 +94,53 @@ while cap.isOpened() and next_frame:
         # extract the foreground mask
         mask = bg_sub.fg_mask(frame)
 
+        # mask = rect.rectify(mask)
+
+        # frame = rect.rectify(frame)
+
+
         # # apply it and obtain an rgb foreground
         # col_foreground = cv2.bitwise_and(frame, frame, mask=mask)
 
-        # extract contours of pedestrians
         contours = ped_det.detect_objects(mask)
+        bboxes = ped_det.get_bboxes()
+
+        if len(bboxes) > 0:
+            ids = ped_tr.assignIDs(bboxes, frame)
+
+        # print(bboxes[0])
+        # if cnt%cnt_upd==0 or cnt<10:
+        #     # extract contours of pedestrians
+        #     coords = bbox2coords(bboxes)
+
+        # if len(coords)==0:
+        #     coords = old_coords
+        #
+        # print("coords")
+        # print(coords)
+
+        # coords = bbox2coords(bboxes)
+        # np.random.shuffle(coords)
+        # coords = ped_tr.update(coords[:,:4].astype(int))
+        # print(coords)
+        #
+        # if len(coords)>0:
+        #     old_coords = coords
+        #
+        # cnt +=1
+        #
+        # predicted_bboxes, ids = coords2bbox(coords)
 
         # update statistics
         stats.update(contours)
 
         # draw contours
         cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+        frame = drawBboxes(bboxes,frame, (0, 0, 255), ids=ids)
+
+        # frame = drawBboxes(predicted_bboxes, frame, (255,0,0), ids=ids)
+        # if len(bboxes)>0:
+        #      cv2.drawContours(frame, bboxes, -1, (0, 0, 255), 2)
 
         # draw ped counts: detected / ground truth
         cv2.putText(frame, 'Objetcs: {}/{}'.format(len(contours), stats.get_curr_truth_counts()),
@@ -59,6 +151,7 @@ while cap.isOpened() and next_frame:
         cv2.imshow('video', frame)
 
         next_frame, frame = cap.read()
+        # time.sleep(0.3)
 
     # q for exit, space for pause
     key = cv2.waitKey(10) & 0xFF
---
+++ src/obj_tracker.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+
+import cv2
+import numpy as np
+from random import randint
+from scipy.spatial import distance_matrix, distance
+from distances import distance_contours, arbitrary_distance_matrix
+
+    #
+    #
+    # kf = cv2.KalmanFilter(4, 2)
+    # kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+    # kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+
+    # def Estimate(self, coordX, coordY):
+    #     """
+    #     This function estimates the position of the object
+    #     :param coordX:
+    #     :param coordY:
+    #     :return:
+    #     """
+    #     measured = np.array([[np.float32(coordX)], [np.float32(coordY)]])
+    #     self.kf.correct(measured)
+    #     predicted = self.kf.predict()
+    #     return predicted
+
+# def getKeys(mydict, value):
+#     """
+#     given a dictionary and a value returns a list of keys associated with that value
+#     :param dict: dicitonary
+#     :param value: value
+#     :return:
+#     """
+#     ret = []
+#
+#     for key, val in mydict.items():
+#         if val == value:
+#             ret.append(key)
+#
+#     return ret
+
+
+class Obj_tracker(object):
+    def __init__(self, frameWidth, frameHeight):
+        # param init
+        # array of kalman filters as motion model
+        # motion prediction and correction
+        self.kfs = []
+        # kf initialization parameters
+        self.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+        self.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+
+        # memory of ids from previous frames
+        # list of dictionaries of type blobID:IDs
+        self.lastIDs = []
+        self.lastIDs.append({0: [0]})
+        # list of last bboxes, each blobID corresponds to the index in the list
+        self.lastBboxes = []
+        # first bbox : whole frame
+        self.lastBboxes.append([0, 0, frameWidth, frameHeight])
+        # centers of bboxes
+        self.lastBboxesCnt = []
+        self.lastBboxesCnt.append(np.array([[frameWidth//2, frameHeight//2]]))
+        # next available ID
+        self.nextID = 1
+        # list of dictionaries of type ID:list of counts
+        self.colHists = []
+        # number of frames to keep track of
+        self.memSize = 2
+
+        # distance threshold for id assignment, max distance a person can travel between 2 frames
+        self.distThreshold = 30
+
+    def assignIDs(self, bboxes, frame):
+        """
+        assigns IDs to the bboxes
+        :param bboxes:
+        :param frame:
+        :return:
+        """
+        bboxes = np.array(bboxes)
+
+        # compute centers of bounding boxes : x + w/2 , y + h//2
+        bboxes_cnt = [list(bboxes[:, 0] + bboxes[:,2]//2), list(bboxes[:, 1] + bboxes[:,3]//2)]
+        # transpose list of lists to get [x,y] couples
+        bboxes_cnt = list(map(list, zip(*bboxes_cnt)))
+
+        # compute distances
+        distances = arbitrary_distance_matrix(bboxes_cnt, self.lastBboxesCnt[-1], distance.euclidean)
+
+        # assigning boxID to each ID
+        # for each previous bbox assign it's id to the closest new box if
+        # the distance between them exceeds a threshold
+        dictids = dict()
+
+        for i in range(len(bboxes)):
+            dictids[i] = []
+
+        dist_T= distances.transpose()
+        # assign previous IDs to new boxes
+        i=0
+        for lastids, dist in zip(self.lastIDs[-1], dist_T):
+            mindist = np.min(dist)
+            if mindist <= self.distThreshold:
+                # assign id of the closest next bbox
+                for el in self.lastIDs[-1][i]:
+                    if el not in dictids[np.argmin(dist)]:
+                        dictids[np.argmin(dist)].append(el)
+            i += 1
+
+        i = 0
+        for bbox, dist in zip(bboxes, distances):
+            if len(dictids[i])==0:
+                mindist = np.min(dist)
+                # in case of splitting of a single blob assign to each blob all previous ids
+                if mindist <= self.distThreshold:
+                    for el in self.lastIDs[-1][np.argmin(dist)]:
+                        if el not in dictids[i]:
+                            dictids[i].append(el)
+                else:
+                    dictids[i].append(self.nextID)
+                    self.nextID += 1
+            i += 1
+
+        # update memory
+        self.lastIDs.append(dictids)
+        self.lastBboxesCnt.append(bboxes_cnt)
+        self.lastBboxes.append(bboxes)
+        if len(self.lastBboxesCnt) > self.memSize:
+            del(self.lastBboxesCnt[0])
+            del(self.lastBboxes[0])
+            del(self.lastIDs[0])
+
+        return list(dictids.values())
+
+
